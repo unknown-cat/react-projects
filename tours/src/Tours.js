28ff9c8adbf8afd9aef77ce31d606e433c3f8f1b
unknown-cat <twilightcat73@yahoo.com>
2021-12-27 21:32:58 +0300

    fix: change name of the classes

--- tours/src/Tours.js
+++ tours/src/Tours.js
@@ -4,9 +4,9 @@ import Tour from './Tour';
 const Tours = ({ tours }) => {
   return (
     <section>
-      <div className="itile">
+      <div className="title">
         <h2>Our tours</h2>
-        <div className="undeline"></div>
+        <div className="underline"></div>
       </div>
       <div>
         {tours.map(tour => (
