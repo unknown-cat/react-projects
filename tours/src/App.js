1045ac9d56ed199ef5cd0c44400f1689f805705b
unknown-cat <twilightcat73@yahoo.com>
2021-12-27 21:26:32 +0300

    feat: implement Tours component
    pass fetched data render tours

--- tours/src/App.js
+++ tours/src/App.js
@@ -28,7 +28,7 @@ function App() {
     <main>
       {loading
         ? <Loading />
-        : <Tours />
+        : <Tours tours={tours}/>
       }
     </main>
   )
--- tours/src/Tours.js
+++ tours/src/Tours.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="itile">
+        <h2>Our tours</h2>
+        <div className="undeline"></div>
+      </div>
+      <div>
+        {tours.map(tour => (
+          <Tour key={tour.id} {...tours} />
+        ))}
+      </div>
+    </section>
+  )
 };
 
 export default Tours;
