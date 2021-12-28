7c0cff651cd82a99ac3cb16df16f8344420583da
unknown-cat <twilightcat73@yahoo.com>
2021-12-28 21:10:15 +0300

    feat: add ability to check next or previous reviews

--- reviews/src/Review.js
+++ reviews/src/Review.js
@@ -6,6 +6,31 @@ const Review = () => {
   const [index, setIndex] = useState(2)
   const { name, job, image, text } = people[index]
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0
+    } 
+    if (number < 0) {
+      return people.length - 1
+    }
+    return number
+  }
+
+  const nextPerson = () => {
+    setIndex(index => {
+      let newIndex = index + 1
+
+      return checkNumber(newIndex)
+    })
+  }
+  const prevPerson = () => {
+    setIndex(index => {
+      let newIndex = index - 1
+    
+      return checkNumber(newIndex)
+    })
+  }
+
   return (
     <article className='review'>
       <div className="img-container">
@@ -18,8 +43,8 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn"><FaChevronLeft /></button>
-        <button className="next-btn"><FaChevronRight /></button>
+        <button className="prev-btn" onClick={prevPerson}><FaChevronLeft /></button>
+        <button className="next-btn" onClick={nextPerson}><FaChevronRight /></button>
       </div>
       <button className="random-btn">surprise-me</button>
     </article>
