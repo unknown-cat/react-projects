e08c03fe91a52c6ed327d91cff87d4435336fe53
unknown-cat <twilightcat73@yahoo.com>
2022-01-05 21:01:30 +0300

    feat: iterate through data and pass it to Question component

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -2,7 +2,23 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [question, setQuestion] = useState(data)
+  return (
+    <main>
+      <div className='container'>
+        <h3>Questions And Answers About Login</h3>
+        <section className='info'>
+          {
+            question.map(question => (
+              <SingleQuestion
+                key={question.id}
+                {...question} />
+            ))
+          }
+        </section>
+      </div>
+    </main>
+  )
 }
 
 export default App;
