3ba12a0c67fbacb1debec326b9a760120dcbdc9d
unknown-cat <twilightcat73@yahoo.com>
2022-01-07 20:15:35 +0300

    feat: add ability to sort items according to category

--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterItems, categories }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => (
+        <button
+          key={index}
+          className="filter-btn"
+          onClick={() => filterItems(category)}>{category}
+        </button>
+      ))}
+    </div>
+  )
 };
 
 export default Categories;
