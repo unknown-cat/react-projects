1a1a3d1e1cb0e26f8265be1f0628235b4d1120e0
unknown-cat <twilightcat73@yahoo.com>
2022-01-09 23:10:31 +0300

    feat: add ability to copy hex color to clipboard

--- color-generator/src/SingleColor.js
+++ color-generator/src/SingleColor.js
@@ -5,13 +5,25 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false)
   const bcg = rgb.join(',')
   const hex = rgbToHex(...rgb)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 1400)
+
+    return () => clearTimeout(timeout)
+  }, [alert])
 
   return (
     <article
       className={`color ${index > 10 && 'color-light'}`}
-      style={{ backgroundColor: `rgb(${bcg})` }}>
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true)
+        navigator.clipboard.writeText(hex)
+      }}>
       <p className='precent-value'>{weight}%</p>
       <p className="color-value">{hex}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
     </article>
   )
 }
