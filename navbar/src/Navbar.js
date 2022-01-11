41153ed4ee4432b01a5a0c79c2e78fa95174883f
unknown-cat <twilightcat73@yahoo.com>
2022-01-11 21:54:33 +0300

    feat: dinamically change height of the container
    depending on how many links we have

--- navbar/src/Navbar.js
+++ navbar/src/Navbar.js
@@ -4,28 +4,38 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  const [showLinks, setShowLinks] = useState(false)
+  const [showLinks, setShowLinks] = useState(true)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  useEffect(() => {
+    const linksHight = linksRef.current.getBoundingClientRect().height
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHight}px`
+    } else {
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [showLinks])
+
   return <nav>
     <div className="nav-center">
       <div className="nav-header">
         <img src={logo} alt="logo" />
-        <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
+        <button
+          className="nav-toggle"
+          onClick={() => setShowLinks(!showLinks)}>
           <FaBars />
         </button>
       </div>
-      {showLinks && (
-        <div className="links-container show-container">
-          <ul className="links">
-            {
-              links.map(({ id, url, text }) => (
-                <li key={id}>
-                  <a href={url}>{text}</a>
-                </li>
-              ))
-            }
-          </ul>
-        </div>
-      )}
+      <div className="links-container" ref={linksContainerRef}>
+        <ul className="links" ref={linksRef}>
+          {links.map(({ id, url, text }) => (
+            <li key={id}>
+              <a href={url}>{text}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
       <ul className="social-icons">
         {
           social.map(({ id, url, icon }) => (
