38047e78921716ac0c6e7a0836f67dbe1470e2ea
unknown-cat <twilightcat73@yahoo.com>
2022-01-16 13:34:35 +0300

    feat: create and pass state and functions

--- sidebar-modal/src/context.js
+++ sidebar-modal/src/context.js
@@ -4,8 +4,32 @@ import React, { useState, useContext } from 'react'
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+    const [isModalOpen, setIsModelOpen] = useState(false)
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+    const openModal = () => {
+        setIsModelOpen(true)
+    }
+    const closeModal = () => {
+        setIsModelOpen(false)
+    }
+
     return (
-        <AppContext.Provider value={'hello'}>
+        <AppContext.Provider value={{
+            isModalOpen,
+            isSidebarOpen,
+            openModal,
+            openSidebar,
+            closeModal,
+            closeSidebar
+        }}
+        >
             {children}
         </AppContext.Provider>
     )
