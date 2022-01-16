ef0fb60e9348538f7b638903e0f33060b55971d1
unknown-cat <twilightcat73@yahoo.com>
2022-01-16 13:38:06 +0300

    feat: change class name according to state, add onclick close modal

--- sidebar-modal/src/Modal.js
+++ sidebar-modal/src/Modal.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { AppContext } from './context'
+
 const Modal = () => {
-  return <div className={`modal-overlay`}>
-    <div className="modal-container">
-      <h3>modal content</h3>
-      <button className="close-modal-btn">
-        <FaTimes />
-      </button>
+  const { isModalOpen, closeModal } = useContext(AppContext)
+  return (
+    <div className={`${isModalOpen
+      ? 'modal-overlay show-modal'
+      : 'modal-overlay'
+      }`}>
+      <div className="modal-container">
+        <h3>modal content</h3>
+        <button className="close-modal-btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default Modal
