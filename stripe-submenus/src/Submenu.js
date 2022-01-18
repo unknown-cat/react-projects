380ff3c6a7facefcc186b6fc7ed6921bf957e770
unknown-cat <twilightcat73@yahoo.com>
2022-01-18 21:00:35 +0300

    feat: display submenu links according to page

--- stripe-submenus/src/Submenu.js
+++ stripe-submenus/src/Submenu.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
 const Submenu = () => {
-  const { isSubmenuOpen, location } = useGlobalContext()
+  const { isSubmenuOpen, location, page: { page, links } } = useGlobalContext()
   const container = useRef(null)
   useEffect(() => {
     const submenu = container.current
@@ -14,7 +14,16 @@ const Submenu = () => {
     <aside
       className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
       ref={container}>
-      Submenu
+      <h4>{page}</h4>
+      <div className={'submenu-center col-2'}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link
+          return <a key={index} href={url}>
+            {icon}
+            {label}
+          </a>
+        })}
+      </div>
     </aside>
   )
 }
--- stripe-submenus/src/context.js
+++ stripe-submenus/src/context.js
@@ -7,6 +7,7 @@ export const AppProvider = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true)
     const [isSubmenuOpen, setIsSubmenuOpen] = useState(true)
     const [location, setLocation] = useState({})
+    const [page, setPage] = useState({ page: '', links: [] })
 
     const openSidebar = () => {
         setIsSidebarOpen(true)
@@ -15,6 +16,8 @@ export const AppProvider = ({ children }) => {
         setIsSidebarOpen(false)
     }
     const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text)
+        setPage(page)
         setLocation(coordinates)
         setIsSubmenuOpen(true)
     }
@@ -29,7 +32,8 @@ export const AppProvider = ({ children }) => {
         openSubmenu,
         closeSidebar,
         closeSubmenu,
-        location
+        location,
+        page
     }}
     >
         {children}
