b9a4200aaaaea5339f5d925f3276218dfc65c5fd
unknown-cat <twilightcat73@yahoo.com>
2022-01-18 21:06:02 +0300

    feat: change submenu width according to number of the links

--- stripe-submenus/src/Submenu.js
+++ stripe-submenus/src/Submenu.js
@@ -3,19 +3,28 @@ import { useGlobalContext } from './context'
 
 const Submenu = () => {
   const { isSubmenuOpen, location, page: { page, links } } = useGlobalContext()
+  const [columns, setColumns] = useState('col-2')
   const container = useRef(null)
   useEffect(() => {
+    setColumns('col-2')
     const submenu = container.current
     const { center, bottom } = location
     submenu.style.left = `${center}px`
     submenu.style.top = `${bottom}px`
-  }, [location])
+
+    if (links.length === 3) {
+      setColumns('col-3')
+    }
+    if (links.length > 3) {
+      setColumns('col-4')
+    }
+  }, [location, location])
   return (
     <aside
       className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
       ref={container}>
       <h4>{page}</h4>
-      <div className={'submenu-center col-2'}>
+      <div className={`submenu-center ${columns}`}>
         {links.map((link, index) => {
           const { label, icon, url } = link
           return <a key={index} href={url}>
