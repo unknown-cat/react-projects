11c6809aa1ca5dc248674db9a4e6d6a5e61f07c0
unknown-cat <twilightcat73@yahoo.com>
2022-01-18 19:42:12 +0300

    feat: create context

--- stripe-submenus/src/context.js
+++ stripe-submenus/src/context.js
@@ -1,2 +1,38 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext()
+
+export const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true)
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+    const openSubmenu = () => {
+        setIsSubmenuOpen(true)
+    }
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false)
+    }
+
+    return <AppProvider.Provider value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        openSubmenu,
+        closeSidebar,
+        closeSubmenu
+    }}
+    >
+        {children}
+    </AppProvider.Provider>
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
