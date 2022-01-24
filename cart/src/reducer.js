e8b74450c7155683b0610ec99fb70c0bf9c55036
unknown-cat <twilightcat73@yahoo.com>
2022-01-24 21:21:04 +0300

    feat: implement function to delete items from cart

--- cart/src/reducer.js
+++ cart/src/reducer.js
@@ -1,4 +1,10 @@
 const reducer = (state, action) => {
+    if (action.type === 'CLEAR_CART') {
+        return {
+            ...state,
+            cart: []
+        }
+    }
     return state
 }
 
