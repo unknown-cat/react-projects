38c09ba68cd02e4db81355d5de04ab70cf49c48f
unknown-cat <twilightcat73@yahoo.com>
2022-01-26 21:05:30 +0300

    feat: get data using fetch

--- cart/src/reducer.js
+++ cart/src/reducer.js
@@ -55,6 +55,19 @@ const reducer = (state, action) => {
             amount
         }
     }
+    if(action.type === 'LOADING') {
+        return {
+            ...state,
+            loading: true
+        }
+    }
+    if(action.type === 'DISPLAY_ITEMS') {
+        return {
+            ...state,
+             cart: action.payload,
+              loading: false
+            }
+    }
     return state
 }
 
