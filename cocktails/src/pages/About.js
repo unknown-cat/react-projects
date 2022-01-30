19e0cc7eeaa2e89538b16c4148d67827897892f2
unknown-cat <twilightcat73@yahoo.com>
2022-01-30 17:20:11 +0300

    feat: add section, title, text

--- cocktails/src/pages/About.js
+++ cocktails/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia neque est nam nobis, possimus maiores fuga, nostrum aspernatur quibusdam labore consequatur doloremque alias repellendus nisi? Nemo voluptas corporis, veritatis id tempora assumenda vitae sint, veniam, mollitia placeat sit aliquid. Excepturi.</p>
+    </section>
   )
 }
 
