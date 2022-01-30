f024687a84dc209bac3c58e73ba05017ed0167cd
unknown-cat <twilightcat73@yahoo.com>
2022-01-30 18:17:45 +0300

    feat: display cocktials

--- cocktails/src/components/CocktailList.js
+++ cocktails/src/components/CocktailList.js
@@ -5,7 +5,6 @@ import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
   const { loading, cocktails } = useGlobalContext()
-
   if (loading) {
     return <Loading />
   }
@@ -17,9 +16,14 @@ const CocktailList = () => {
     )
   }
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+    <section className='section'>
+    <h2 className='section-title'>cocktails</h2>
+    <div className='cocktails-center'>
+        {cocktails.map(item => {
+          return <Cocktail key={item.id} {...item}/>
+        })}
+      </div>
+    </section>
   )
 }
 
--- cocktails/src/context.js
+++ cocktails/src/context.js
@@ -26,6 +26,7 @@ const AppProvider = ({ children }) => {
             glass: strGlass,
           }
         })
+        setCocktails(newCocktails)
       } else {
         setCocktails([])
       }
